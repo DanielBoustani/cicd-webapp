5a24df0bc28978f993e09771d4d04e05849919b7
unknown <daniel.boustani@student.hogent.be>
2023-12-05 14:59:10 +0100

    Second workflow (2)

--- persistence/index.js
+++ persistence/index.js
@@ -1,6 +1,6 @@
 import * as mongodb from './mongodb.js';
 import * as sqlite from './sqlite.js';
 
-console.log("Demo")
+console.log('Demo');
 const database = process.env.MONGO_URL ? mongodb : sqlite;
 export default database;
